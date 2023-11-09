49f4a60371e3f911b42aabe8746904e7ef63ffd6
Fabiana Iaschombek Costa <105210760+iaschombek@users.noreply.github.com>
2023-11-09 08:18:52 -0300

    som

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert("Eu amo a Camila")
+function tocaSom(idElementoAudio){
+document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll("tecla");
+
+let contador = 0;
+while(contador<listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom("#som_tecla_aplausos");
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+    console.log(contador);
+
+}
